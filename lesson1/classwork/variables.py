cdb3785fb448e7569f26707a5e83792c48d5f0ee
averyvshaw <avery.v.shaw@gmail.com>
2026-02-18 04:40:14 +0000

    Notes taken during class

---
+++ lesson1/classwork/variables.py
@@ -0,0 +1,6 @@
+x = 5 # interger: whole numbers without decimal
+print(x)
+favorite_food = "udon" # string: text enclosed in text 
+print(favorite_food)
+y = 3.25 # float: number with decimal point
+print(y,"udon", 4)
