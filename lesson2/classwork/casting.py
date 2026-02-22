d15ce78606bad92a1741fd8e9ad5ffa63c2cf8ee
averyvshaw <avery.v.shaw@gmail.com>
2026-02-21 17:30:19 -0800

    Update casting.py

---
+++ lesson2/classwork/casting.py
@@ -0,0 +1,11 @@
+num_str = "12"
+num_int = int (num_str)
+print (num_str + 1)
+
+num_str = "3.14"
+num_float = float (num_str)
+print (num_float)
+
+float_num = 9.99
+int_num = (float_num)
+print (int_num)
